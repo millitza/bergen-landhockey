9d10d8ce41caafed7f68df204ea1eedf5a1e79ea
Millitza Kroonenberg <mdk@kvikna.no>
2025-01-23 10:39:08 +0100

    ci: fix deployment for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://millitza.github.io",
-    base: "bergen-landhockey",
+    site: "https://bergenlandhockey.no",
 });
