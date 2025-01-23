965f83e7ccfc03e96ceb6583accb8b6db6f1e9da
Millitza Kroonenberg <mdk@kvikna.no>
2025-01-23 15:59:04 +0100

    style: improve section colors

--- src/utils/sectionColors.ts
+++ src/utils/sectionColors.ts
@@ -1,8 +1,8 @@
 export const getSectionColor = (index: number) => {
     if (index % 3 == 0) {
         return {
-            bgColor: "--color-purple",
-            textColor: "--color-gray"
+        bgColor: "--color-light-gray",
+        textColor: "--text-primary-color"
         }
     }
     if (index % 2 == 0) {
@@ -11,14 +11,8 @@ export const getSectionColor = (index: number) => {
             textColor: "--text-primary-color"
         }
     }
-    if (index % 1 == 0) {
-        return {
-            bgColor: "--color-white",
-            textColor: "--text-primary-color"
-        }
-    }
     return {
-        bgColor: "--color-light-gray",
+        bgColor: "--color-white",
         textColor: "--text-primary-color"
     }
 }
